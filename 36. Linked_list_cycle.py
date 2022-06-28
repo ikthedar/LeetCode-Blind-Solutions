be73123661d1b955634810dc4d42a07193f3cf68
ikthedar <ikchy1994@gmail.com>
2022-06-28 13:22:25 +0600

    Comment added

--- 36. Linked_list_cycle.py
+++ 36. Linked_list_cycle.py
@@ -1,3 +1,7 @@
+## We are using FLoyd's tortoise and Hare algorithm here instead of usign Hash Set for better time complexity.
+## Time = O(n)
+
+
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         slow, fast = head, head
