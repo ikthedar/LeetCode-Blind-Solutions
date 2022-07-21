de17c3eba699400101e7f59cb020f0e11cbc63af
Ikthedar Chowdhury <ikchy1994@gmail.com>
2022-07-21 21:46:33 +0600

    Comment added to understand better

--- 62. Encode_Decode_Strings.py
+++ 62. Encode_Decode_Strings.py
@@ -1,12 +1,14 @@
 class Solution:
 
-    def encode(self, strs):
+    def encode(self, strs): # we are taking a list of strings
         # write your code here
-        res = ""
-        for s in strs:
+        res = "" # we are creating a single string
+        
+        for s in strs: # will travers each string "s" from the list of strings "strs"
             res += str(len(s)) + "#" + s
         return res
 
+      
     def decode(self, str):
         # write your code here
         res, i = [], 0
@@ -15,7 +17,7 @@ class Solution:
             j = i
             while str[j] != "#":
                 j += 1
-            length = int(str[i:j])
-            res.append(str[j + 1 : j + 1 + length])
+            length = int(str[i:j]) # will take the digit between i and j
+            res.append(str[j + 1 : j + 1 + length]) # since this is a list, we are using .append
             i = j + 1 + length
         return res
