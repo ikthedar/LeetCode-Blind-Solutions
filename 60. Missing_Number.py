93d038d0559ee84081488cb3bae93010d4d0c075
Ikthedar Chowdhury <ikchy1994@gmail.com>
2022-07-30 00:12:10 +0600

    Update and rename Missing number.py to 60. Missing_Number.py

--- 60. Missing_Number.py
+++ 60. Missing_Number.py
@@ -1,5 +1,13 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        # using XOR operation
+        res = len(nums)
+        for i in range(len(nums)):
+            res ^= i ^ nums[i]
+        return res
+
+-----
+     # using SUM method   
         res = len(nums)
 
         for i in range(len(nums)):
