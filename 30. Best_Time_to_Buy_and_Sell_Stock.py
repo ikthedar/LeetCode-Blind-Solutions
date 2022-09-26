53403c9df55f602a094ebf2b904922bef1307f32
Ikthedar Chowdhury <ikchy1994@gmail.com>
2022-09-26 20:03:24 +0600

    Added my solution

--- 30. Best_Time_to_Buy_and_Sell_Stock.py
+++ 30. Best_Time_to_Buy_and_Sell_Stock.py
@@ -2,8 +2,25 @@
 # No extra memory is used since we just used pointers, no array. So, Space = O(1).
 # Time complexity = O(n), since time is linear here, didn't use any brute force method.
 
-
+# my solution
 class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxP = 0
+        
+        while r < len(prices):
+            if prices[l] > prices[r]:
+                l = r
+                r = r + 1
+            else:
+                curP = prices[r] - prices[l]
+                maxP = max(curP, maxP)
+                r = r + 1
+                
+        return maxP
+
+# NeetCode example
+'''class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         l, r = 0, 1 # Left=buying, right=selling
         maxP = 0
@@ -16,4 +33,4 @@ class Solution:
             else:
                 l = r
             r +=1
-        return maxP
+        return maxP'''
