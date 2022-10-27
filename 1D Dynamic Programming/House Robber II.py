96b483e924fb9618ff7f2ce68ee9583e2a678a40
Ikthedar Chowdhury <ikchy1994@gmail.com>
2022-10-27 23:50:08 +0600

    Update House Robber II.py

--- 1D Dynamic Programming/House Robber II.py
+++ 1D Dynamic Programming/House Robber II.py
@@ -1,14 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
-        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
+        # nums[1:] is array except the first one; nums[:-1] array except the last one
+        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1])) 
         
         
     def helper(self, nums):
             robBefAdj, robAdj = 0, 0
             
             for n in nums:
-                newRob = max(robBefAdj + n, robAdj)
+                newRob = max(robBefAdj + n, robAdj) #  eg: [1, 3, 4, 5, 2] for 5 newRob ... 
                 robBefAdj = robAdj
                 robAdj = newRob
                 
