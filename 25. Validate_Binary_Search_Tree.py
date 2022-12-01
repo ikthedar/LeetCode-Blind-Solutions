f5a42290df9153f1f2d4c717a7dbadfa00cea000
Ikthedar Chowdhury <ikchy1994@gmail.com>
2022-12-01 22:41:34 +0600

    Update 25. Validate_Binary_Search_Tree.py

--- 25. Validate_Binary_Search_Tree.py
+++ 25. Validate_Binary_Search_Tree.py
@@ -1,13 +1,15 @@
 class Solution:
-    def isValidBST(self, root: TreeNode) -> bool:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        
+        # creating a helper function inside the original function
         def valid(node, left, right):
             if not node:
-                return True
+                return True 
+            
             if not (node.val < right and node.val > left):
                 return False
-
-            return valid(node.left, left, node.val) and valid(
-                node.right, node.val, right
-            )
-
+            
+            return (valid(node.left, left, node.val) and
+                    valid(node.right, node.val, right))
+        
         return valid(root, float("-inf"), float("inf"))
